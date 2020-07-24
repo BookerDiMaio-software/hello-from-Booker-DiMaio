681197da9a24c7c2c72466af71953526bbb3eba1
Kyle Yerty <kyle.yerty@bookerdimaio.com>
2020-07-24 15:48:05 -0400

    Made start.sh output more readable.

--- start.sh
+++ start.sh
@@ -5,17 +5,24 @@ set -e
 echo "Testing..."
 ./mvnw verify
 
+echo
 echo "Packaging..."
 ./mvnw -DskipTests package
 
+echo
 echo "Building local Docker image..."
 ./mvnw -DskipTests com.google.cloud.tools:jib-maven-plugin:dockerBuild -Dimage=java-kubernetes-example:v1
 
+echo
 echo "Deploying to Kubernetes..."
 kubectl apply -f deployment.yml
 
+echo
 echo "Starting Minikube tunnel..."
-minikube tunnel &
+minikube tunnel > /dev/null &
 echo $! > tunnel.pid
 
+sleep 15
+
+echo
 echo "The example is ready! Please navigate to http://localhost:8080"
