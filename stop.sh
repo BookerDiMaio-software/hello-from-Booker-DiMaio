0b6e389db72a72936b0bee71043d677f74bee8ba
Kyle Yerty <kyle.yerty@bookerdimaio.com>
2020-07-24 16:23:19 -0400

    Removing docker unlink command from stop.sh since it doesn't seem to work.

--- stop.sh
+++ stop.sh
@@ -11,6 +11,3 @@ if [ -f "tunnel.pid" ]; then
   kill -- -"$(ps -o pgid= "$(cat "tunnel.pid")" | grep -o [0-9]*)"
   rm "tunnel.pid"
 fi
-
-echo "Unlinking Kubernetes to local Docker images..."
-eval "$(minikube docker-env -u)"
